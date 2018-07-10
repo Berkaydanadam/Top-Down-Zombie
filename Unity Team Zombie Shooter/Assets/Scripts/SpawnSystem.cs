d9876750e01a57f294540dfc59b6a73e3edbafb3
flurreN <Philipsabri@gmail.com>
2018-07-10 21:12:36 +0100

    IEnumerator Spawn system with time
    fixed error
    switched to IEnumerator to have time inbetween spawns
    X amount of spawn point set at the begining of wave and then spawn zombies around these points with a time delay
    
    changed some values for SpawnSystem in the editor(scene)

--- Unity Team Zombie Shooter/Assets/Scripts/SpawnSystem.cs
+++ Unity Team Zombie Shooter/Assets/Scripts/SpawnSystem.cs
@@ -10,7 +10,10 @@ public class SpawnSystem : MonoBehaviour {
     public GameObject zombiePrefab; //Single prefab for the zombies, rewrite the code if we would use different zombie prefabs
     public GameObject Player;
 
+    public List<Vector3> spawnPoints = new List<Vector3>(); //saves spawn point in here
+
     public int wave = 0; //wave number
+    public int totalSpawn = 5; //amount of random spawn points across the level
     public int totalZombies; //total zombies in the current wave
     public int aliveZombies; //alive zombiesi in the current wave
     public float minimumDistance = 5.0f; //min distance for zombie spawn from player
@@ -24,7 +27,7 @@ public class SpawnSystem : MonoBehaviour {
     {
         //Spawn first wave of zombies
         wave = 1;
-        Spawn();
+        StartCoroutine(Spawn());
 	}
 	
 	// Update is called once per frame
@@ -33,15 +36,19 @@ public class SpawnSystem : MonoBehaviour {
         //check for alive zombies
         aliveZombies = GameObject.FindGameObjectsWithTag("Zombie").Length;
 
+        
         if ((aliveZombies == 0 || aliveZombies < 0) && nextWave == false)
         {
             nextWave = true;
             StartCoroutine(NextWave());
         }
+        
     }
 
-    IEnumerator Spawn()
+    public IEnumerator Spawn()
     {
+        //clear spawnPoints list
+        spawnPoints.Clear();
         if (spawnEnable == true && aliveZombies == 0)
         {
             float randomX = 0;
@@ -50,17 +57,15 @@ public class SpawnSystem : MonoBehaviour {
             spawnEnable = false; //Set spawn to false during spawning so it wont be triggered multiple times
             totalZombies = wave * 5; //Add the right amount of zombies each spawn round
 
-            for (int i = 0; i < totalZombies; i++)
+            //decide spawn points here
+            for (int i = 0; i < totalSpawn; i++)
             {
-                /*This can definitely be done in a better way*/
-                //This will give the zombies different locations for all of them
                 do
                 {
-
-                    //eliminates every position 5 feet from the player ----- minimum distance in this
+                    //eliminates every positions close to the player according to maxi- and minimumumDistance
 
                     randomX = Random.Range(maximumDistance * -1, maximumDistance); //change this for maximum distance  
-                    if(randomX + Player.gameObject.transform.position.x > 5.0f || randomX < -5.0f)
+                    if (randomX + Player.gameObject.transform.position.x > 5.0f || randomX < -5.0f)
                     {
                         //if X is in the right values it will loop and check for Z 
                         do
@@ -76,17 +81,32 @@ public class SpawnSystem : MonoBehaviour {
                     }
                 } while (true);
 
+                //ranxomX and RandomZ is now the random position for one of the spawns
                 randomX = randomX + Player.gameObject.transform.position.x; //set values for the spawn positions
                 randomZ = randomZ + Player.gameObject.transform.position.y;
-                Instantiate(zombiePrefab, new Vector3(randomX, 1, randomZ), Quaternion.identity);
 
-                Debug.Log("Spawn at X: " + randomX + " and Y: " + randomZ);
+                spawnPoints.Add(new Vector3(randomX, 1, randomZ));
+
+
+                Debug.Log("Spawn point at X: " + randomX + " and Y: " + randomZ);
+
             }
             Debug.Log("Spawning complete");
+
+            //SPAWN ZOMBIES HERE 
+
+            //Spawn X amount of zombies on this spawn point
+            Instantiate(zombiePrefab, new Vector3(randomX, 1, randomZ), Quaternion.identity);
+
+            foreach(Vector3 spawn in spawnPoints)
+            {
+                yield return new WaitForSeconds(1.5f);
+                Instantiate(zombiePrefab, new Vector3(spawn.x, spawn.y, spawn.z), Quaternion.identity);
+            }
             spawnEnable = true;
+
         }
     }
-
     IEnumerator NextWave()
     {
         Debug.Log("Wait 5 sec for next wave");
@@ -94,7 +114,10 @@ public class SpawnSystem : MonoBehaviour {
         Debug.Log("Wave: " + wave);
         wave++;
         nextWave = false;
-        Spawn();
+        StartCoroutine(Spawn());
     }
+    public void test()
+    {
 
+    }
 }
