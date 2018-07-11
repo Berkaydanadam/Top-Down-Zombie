cc62ca98bab45152b40f130fd6de2d0e3e625d63
icebergHugger <kolbeinn.floki@gmail.com>
2018-07-11 03:27:33 +0000

    Zombies follow
    Now the zombies follow the player around.

---
+++ Unity Team Zombie Shooter/Assets/Scripts/zombieScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class zombieScript : MonoBehaviour {
+
+    GameObject player;
+    NavMeshAgent agent;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+	void Update () {
+        agent.destination = player.transform.position;
+    }
+}
