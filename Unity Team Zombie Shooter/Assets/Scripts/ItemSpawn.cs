f50d58deea00b99f716a9e1175f0fead361c8ac2
flurreN <Philipsabri@gmail.com>
2018-07-12 04:09:30 +0100

    Basic Item Spawn System
    changed name on ZombieSpawn Script
    Added a basic item spawn system
    
    removed debug.log from ZombieSpawn script

---
+++ Unity Team Zombie Shooter/Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour {
+
+    public GameObject Player;
+    public List<GameObject> items = new List<GameObject>();
+
+    public float nextSpawn = 0; //Time in seconds for the next spawn of item
+    public int itemNumber; //number from list, starts at 0;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnDecider());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void SpawnItem()
+    {
+        itemNumber = Random.Range(0, items.Count); //decide what type of item to spawn - gets a random item from the item list.
+
+        //random location from player
+        Instantiate(items[itemNumber], new Vector3(Random.Range(-10.0f, 10.0f), 1, Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+        //Debug.Log("Spawning: " + items[itemNumber].name);
+        StartCoroutine(SpawnDecider());
+    }
+    IEnumerator SpawnDecider()
+    {
+        nextSpawn = Random.Range(0, 5); //set a random time for the next spawn
+        yield return new WaitForSeconds(nextSpawn);
+        SpawnItem();
+    }
+
+}
