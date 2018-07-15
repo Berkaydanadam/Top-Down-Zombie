108b47d2c9db4015ca505b535040453238805df1
flurreN <Philipsabri@gmail.com>
2018-07-15 23:40:14 +0100

    Item Pickup Inheritance and Interface

---
+++ Unity Team Zombie Shooter/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickup
+{
+    void Destroy();
+    void OnTriggerEnter(Collider other);
+    void PickUp();
+}
