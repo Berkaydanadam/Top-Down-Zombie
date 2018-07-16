3014c3a8eacfebdc5c14ff7aaffd153f9c6b091f
flurreN <Philipsabri@gmail.com>
2018-07-16 03:06:25 +0100

    Item Spawn
    Bug fix
    max items on ground
    respawns items
    removes item if max item is on ground and spawn a new one

--- Unity Team Zombie Shooter/Assets/Scripts/ItemSpawn.cs
+++ Unity Team Zombie Shooter/Assets/Scripts/ItemSpawn.cs
@@ -13,6 +13,8 @@ public class ItemSpawn : MonoBehaviour {
     public int itemNumber; //number from list, starts at 0;
     public int maxItemsOnGround; //max items on ground allowed
     public int totalItemsOnGround; //total items on the ground at the moment
+    public int maxTime = 0;
+    public int minTime = 0;
 
     // Use this for initialization
     void Start () {
@@ -26,76 +28,51 @@ public class ItemSpawn : MonoBehaviour {
     public void SpawnItem()
     {
         itemNumber = Random.Range(0, items.Count); //decide what type of item to spawn - gets a random item from the item list.
-        if(totalItemsOnGround < maxItemsOnGround)
+        if (totalItemsOnGround < maxItemsOnGround)
         {
-            Debug.Log("Spawns item");
-
             //spawn item
             Instantiate(items[itemNumber], new Vector3(Random.Range(-10.0f, 10.0f), 1, Random.Range(-10.0f, 10.0f)), Quaternion.identity);
             totalItemsOnGround++;
-            Debug.Log("++");
             updateItemOnGroundList();
-
-
-        }
-        else if(totalItemsOnGround >= maxItemsOnGround) //if there is too many items on the ground
-        {
-            //remove one random 
-            int itemsOnGroundListCount = itemsOnGroundList.Count - 1;
-            if(itemsOnGroundListCount < 0)
-            {
-                itemsOnGroundListCount = 0;
-                updateItemOnGroundList();
-            }
-            int removingRandomItem = Random.Range(0, itemsOnGroundListCount);
-            Destroy(itemsOnGroundList[removingRandomItem]);
-            itemsOnGroundList.RemoveAt(removingRandomItem);
-            totalItemsOnGround--;
-            Debug.Log("destroy + removeat + go to spawnitem()");
-            SpawnItem();
         }
         else
         {
-            updateItemOnGroundList();
-            Debug.Log("else updateitemongroundlist()");
+            //remove item
+            int removeRandom = Random.Range(0, itemsOnGroundList.Count);
+
+            Destroy(itemsOnGroundList[removeRandom]); //destroys item
+            itemsOnGroundList.RemoveAt(removeRandom); //remove the position from list
+            totalItemsOnGround--; 
+            StartCoroutine(SpawnDecider());
         }
 
-        //Debug.Log("Spawning: " + items[itemNumber].name);
-        StartCoroutine(SpawnDecider());
     }
     IEnumerator SpawnDecider()
     {
-        Debug.Log("SpawnDecider() starter from IE");
-        nextSpawnTime = Random.Range(0, 5); //set a random time for the next spawn
-        yield return new WaitForSeconds(2);
+        nextSpawnTime = Random.Range(minTime, maxTime); //set a random time for the next spawn
+        yield return new WaitForSeconds(nextSpawnTime);
         SpawnItem();
     }
     public void updateItemOnGroundList()
     {
-        if (itemsOnGroundList.Count > maxItemsOnGround || itemsOnGroundList.Count < maxItemsOnGround)
-        {
-            itemsOnGroundList.Clear();
-            itemsOnGroundList.AddRange(GameObject.FindObjectsOfType<GameObject>()); //gets all gameobjects in scene
+        itemsOnGroundList.Clear();
+        itemsOnGroundList.AddRange(GameObject.FindObjectsOfType<GameObject>()); //gets all gameobjects in scene
 
-            Debug.Log("clear and addrange");
-        }
-
-        //removes every gameobject that does not contain Item from list
         foreach (GameObject item in itemsOnGroundList.ToArray())
         {
-            if(item != null)
+            if (item != null) //check all spaces thats not null
             {
-                if (item.tag.Contains("Item") == false)
+                if (item.tag.Contains("Item") == false) //if its not an Item it will be removed
                 {
                     itemsOnGroundList.Remove(item);
-                    Debug.Log("remove item from list");
                 }
             }
-            else if(item == null)
+            else if (item == null) //remove empty spaces
             {
-                itemsOnGroundList.Clear();
-                Debug.Log("clear list");
+                itemsOnGroundList.Remove(item);
             }
         }
+        StartCoroutine(SpawnDecider());
     }
+
 }
