94ebb80bb9571e6c43069255ea600099135c4f04
flurreN <Philipsabri@gmail.com>
2018-07-16 04:25:05 +0100

    basic day and night

---
+++ Unity Team Zombie Shooter/Assets/Scripts/DayNight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+    [Range(0.0f, 23.9f)]
+    public float test = 0;
+    float x;
+    float y;
+
+    public bool cycle = false; //goes from night to day if set to true
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cycle == true)
+        {
+            //day and night cycle
+        }
+        x =  15 * test;
+        y =  3 * test;
+        transform.rotation = Quaternion.Euler(x, y, 0);
+
+    }
+}
